4e939444c01f5dff39e6a5e63936ca43a44e392a
Quirogabrayan <bstivenquiroga@ucundinamarca.edu.co>
2025-03-16 20:35:20 -0500

    finalizacion codigo

--- src/main/java/org/example/TorreDeHanoi.java
+++ src/main/java/org/example/TorreDeHanoi.java
@@ -1,3 +1,5 @@
+package org.example;
+
 public class TorreDeHanoi {
 
     public static void torreDeHanoi(int n, char origen, char destino, char auxiliar) {
@@ -6,5 +8,14 @@ public class TorreDeHanoi {
             System.out.println("Mover disco 1 de " + origen + " a " + destino);
             return;
         }
-
         torreDeHanoi(n - 1, origen, auxiliar, destino);
+
+        System.out.println("Mover disco " + n + " de " + origen + " a " + destino);
+
+        torreDeHanoi(n - 1, auxiliar, destino, origen);
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        torreDeHanoi(n, 'A', 'C', 'B');
+    }
+}
